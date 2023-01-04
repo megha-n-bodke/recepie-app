c00cb9732e93c43a246ad23e73f9708ff905661b
Megha Bodke <megha.n.bodke@gmail.com>
2023-01-04 13:50:07 +0530

    Add food and delete food functionality

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,5 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[edit update destroy ]
-
+  before_action :set_food, only: %i[edit update destroy]
 
   def index
     @foods = Food.includes(:user).all
@@ -11,21 +10,19 @@ class FoodsController < ApplicationController
   end
 
   def shopping_list
-   @foods = Food.all
-   @sum = sumtotal(@foods)
-   @qty = quantity_total(@foods)
+    @foods = Food.all
+    @sum = sumtotal(@foods)
+    @qty = quantity_total(@foods)
   end
 
-  def edit
-  end
-
+  def edit; end
 
   def create
     @food = Food.new(food_params)
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to foods_url, notice: "Food was successfully created." }
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +35,7 @@ class FoodsController < ApplicationController
   def update
     respond_to do |format|
       if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: "Food was successfully updated." }
+        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
         format.json { render :show, status: :ok, location: @food }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,34 +49,36 @@ class FoodsController < ApplicationController
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: "Food was successfully destroyed." }
+      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_food
-      @food = Food.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def food_params
-      params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_food
+    @food = Food.find(params[:id])
+  end
 
-    def sumtotal(obj)
-       sum = 0
-       obj.each do |val|
-         sum += val.price
-       end
-       sum
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
+
+  def sumtotal(obj)
+    sum = 0
+    obj.each do |val|
+      sum += val.price
     end
-    def quantity_total(obj)
-      sum = 0
-      obj.each do |val|
-        sum += val.quantity.to_i
-      end
-      sum
-   end
+    sum
+  end
+
+  def quantity_total(obj)
+    sum = 0
+    obj.each do |val|
+      sum += val.quantity.to_i
+    end
+    sum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
-  resources :users
+  resources :foods
+  resources :users do
+  resources :foods
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
